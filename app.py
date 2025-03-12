9ce68fcc5e3e06b491763af3542099aa887ad8a3
Yousuf Sharif Fahim <fahim.cse.bubt@gmail.com>
2025-03-13 05:31:43 +0600

    title edit, more abb

--- app.py
+++ app.py
@@ -6,17 +6,50 @@ import re
 # Define abbreviation dictionaries
 directional_abbr = {
     'N': 'North', 'S': 'South', 'E': 'East', 'W': 'West',
-    'NE': 'Northeast', 'NW': 'Northwest', 'SE': 'Southeast', 'SW': 'Southwest'
+    'NE': 'Northeast', 'NW': 'Northwest', 'SE': 'Southeast', 'SW': 'Southwest',
+    'NORTH': 'North', 'SOUTH': 'South', 'EAST': 'East', 'WEST': 'West',
+    'NORTHEAST': 'Northeast', 'NORTHWEST': 'Northwest', 'SOUTHEAST': 'Southeast', 'SOUTHWEST': 'Southwest'
 }
 
 street_type_abbr = {
     'St': 'Street', 'Ave': 'Avenue', 'Blvd': 'Boulevard', 'Rd': 'Road',
-    'Ln': 'Lane', 'Dr': 'Drive', 'Ct': 'Court', 'Pl': 'Place'
+    'Ln': 'Lane', 'Dr': 'Drive', 'Ct': 'Court', 'Pl': 'Place',
+    'Sq': 'Square', 'Ter': 'Terrace', 'Cir': 'Circle', 'Pkwy': 'Parkway',
+    'Trl': 'Trail', 'Trce': 'Trace', 'Hwy': 'Highway', 'Ctr': 'Center',
+    'Spg': 'Spring', 'Lk': 'Lake', 'Aly': 'Alley', 'Bnd': 'Bend', 'Brg': 'Bridge',
+    'Byu': 'Bayou', 'Clf': 'Cliff', 'Cor': 'Corner', 'Cv': 'Cove', 'Crk': 'Creek',
+    'Xing': 'Crossing', 'Gdn': 'Garden', 'Gln': 'Glen', 'Grn': 'Green',
+    'Hbr': 'Harbor', 'Holw': 'Hollow', 'Is': 'Island', 'Jct': 'Junction',
+    'Knl': 'Knoll', 'Mdws': 'Meadows', 'Mtn': 'Mountain', 'Pass': 'Pass',
+    'Pt': 'Point', 'Rnch': 'Ranch', 'Shrs': 'Shores', 'Sta': 'Station',
+    'Vly': 'Valley', 'Vw': 'View', 'Wlk': 'Walk',
+    'Anx': 'Annex', 'Arc': 'Arcade', 'Av': 'Avenue', 'Bch': 'Beach',
+    'Bg': 'Burg', 'Bgs': 'Burgs', 'Blf': 'Bluff', 'Blfs': 'Bluffs',
+    'Bot': 'Bottom', 'Br': 'Branch', 'Brk': 'Brook', 'Brks': 'Brooks',
+    'Btw': 'Between', 'Cmn': 'Common', 'Cmp': 'Camp', 'Cnyn': 'Canyon',
+    'Cpe': 'Cape', 'Cswy': 'Causeway', 'Clb': 'Club', 'Con': 'Corner',
+    'Cors': 'Corners', 'Cp': 'Camp', 'Cres': 'Crescent', 'Crst': 'Crest',
+    'Xrd': 'Crossroad', 'Ext': 'Extension', 'Falls': 'Falls', 'Frk': 'Fork',
+    'Frks': 'Forks', 'Ft': 'Fort', 'Fwy': 'Freeway', 'Gdns': 'Gardens',
+    'Gtway': 'Gateway', 'Hghts': 'Heights', 'Hvn': 'Haven', 'Hd': 'Head',
+    'Hlls': 'Hills', 'Inlt': 'Inlet', 'Jcts': 'Junctions', 'Ky': 'Key',
+    'Kys': 'Keys', 'Lndg': 'Landing', 'Lgt': 'Light', 'Lgts': 'Lights',
+    'Lf': 'Loaf', 'Mnr': 'Manor', 'Mls': 'Mills', 'Mssn': 'Mission',
+    'Mt': 'Mount', 'Nck': 'Neck', 'Orch': 'Orchard', 'Oval': 'Oval',
+    'Prk': 'Park', 'Pkwys': 'Parkways', 'Pln': 'Plain', 'Plz': 'Plaza',
+    'Prt': 'Port', 'Pr': 'Prairie', 'Rad': 'Radial', 'Rdg': 'Ridge',
+    'Riv': 'River', 'Rdge': 'Ridge', 'Run': 'Run', 'Shl': 'Shoal',
+    'Shls': 'Shoals', 'Skwy': 'Skyway', 'Spgs': 'Springs', 'Spur': 'Spur',
+    'Strm': 'Stream', 'Stm': 'Stream', 'Trfy': 'Terrace', 'Trwy': 'Throughway',
+    'Tpke': 'Turnpike', 'Un': 'Union', 'Vlg': 'Village', 'Vis': 'Vista',
+    'Way': 'Way', 'Expy': 'Expressway', 'Frwy': 'Freeway', 'Tunl': 'Tunnel'
 }
 
 unit_abbr = {
     'Apt': 'Apartment', 'Ste': 'Suite', 'Bldg': 'Building',
-    'Unit': 'Unit', 'Rm': 'Room', 'Fl': 'Floor'
+    'Unit': 'Unit', 'Rm': 'Room', 'Fl': 'Floor', 'Dep': 'Department',
+    'Ofc': 'Office', 'Sp': 'Space', 'Lot': 'Lot', 'Trlr': 'Trailer',
+    'Hangar': 'Hangar', 'Slip': 'Slip', 'Pier': 'Pier', 'Dock': 'Dock'
 }
 
 def clean_address(address):
@@ -43,7 +76,7 @@ def clean_address(address):
         return address
 
 # Streamlit UI
-st.title("📍 Address Cleaner for Leads")
+st.title("📍 Address Cleaner")
 
 # Add hyperlink for sample CSV download
 st.markdown("[📥 Download Sample CSV](https://drive.google.com/file/d/19CdaLPNq7SUY1ROx0RgLdFD9gQI9JrSh/view?usp=sharing)", unsafe_allow_html=True)
